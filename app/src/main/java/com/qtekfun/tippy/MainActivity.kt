b94445fec20e6d9df70195b9d06b496748584f06
Qtekfun <qtekfun@gmail.com>
2024-06-13 21:26:06 +0200

    feat: play with styles

--- app/src/main/java/com/qtekfun/tippy/MainActivity.kt
+++ app/src/main/java/com/qtekfun/tippy/MainActivity.kt
@@ -1,29 +1,37 @@
 package com.qtekfun.tippy
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.qtekfun.tippy.ui.theme.TippyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TippyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                    )
+                Column {
+                    Cell()
                 }
             }
         }
@@ -31,17 +39,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun CellTexts(text: String, style: TextStyle) {
+    Column(modifier = Modifier.padding(8.dp)) {
+        Text(
+            "This is a cell with text: ",
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = text, style = style, color = MaterialTheme.colorScheme.secondary)
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Cell() {
+    Row(modifier = Modifier
+        .background(MaterialTheme.colorScheme.primaryContainer)
+        .padding(4.dp)) {
+        Image(
+            modifier = Modifier
+                .size(64.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(8.dp),
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Test Iamge"
+        )
+        Spacer(modifier = Modifier.width(6.dp))
+        CellTexts("I've got it", MaterialTheme.typography.labelLarge)
+    }
+}
+
+@Preview()
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun GreetingPreview() {
     TippyTheme {
-        Greeting("Android")
+        Column {
+            Cell()
+        }
     }
 }
